c19fdf42a123c64502c4b2715fe2f8cca5b2d601
anthonyyang48 <wangrusherry12@gmail.com>
2017-10-11 21:58:44 +0000

    Changes

---
+++ index.js
@@ -0,0 +1,21 @@
+var number = Math.random(0, 100) * 100;
+
+console.log(number);
+
+var now = "10/11/17 5:30 p.m."
+
+var names = ["Alex", "Kevin", "Aziz", "Mahfuz", "Angelo", "Fernado", "Imani", "Kayahama", "Xira", "Coral", ]
+
+
+/*var teachers = 
+           {"name": "Pete",
+            "name": "Moe",
+            "name": "Alex",
+            "name": "Abe",
+             };*/
+             
+ function log (name){
+  console.log(name);
+ };
+ 
+log('Abe');
